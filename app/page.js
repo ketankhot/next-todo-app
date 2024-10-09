efd634da4196213bcbbda47b2c657a03e39e7a8a
Ketan Khot <deltacodingketan@gmail.com>
2024-10-09 20:58:11 +0530

    Init

--- app/page.js
+++ app/page.js
@@ -2,10 +2,9 @@
 import Todo from "@/Components/Todo";
 import axios from "axios";
 import React from "react";
-import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const page = () => {
   const [formData, setFormData] = useState({
